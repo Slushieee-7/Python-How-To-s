af3292fab5dc2cfb84d958dd702d1529750e722e
Rey Delos Reyes <reyjoshua5922@gmail.com>
2025-01-14 09:58:29 +0800

    tbc

---
+++ Python Basics/Basics1.py
@@ -0,0 +1,24 @@
+#this is how u can comment in python
+
+#this is how to print in python
+print("Part 1: Hello, World!")
+
+#indentation is important in python
+#indentation is used to define a block of code
+if 5 > 3:
+    print("Part 2: 5 is greater than 3")  #this would be an error if there is no indention
+
+#variables in python
+x = 4
+y = 5
+print ("Part 3:", x + y)  #this would print 9
+                             #to concatenate strings or numbers together, instead of using +, in python, we use the comma (,)
+
+#u can also use data types to specify the data types of the variable
+x = int(5) 
+y = float(2.5)
+z = str(20)
+print ("Part 4:", "int:", x, "float:", y, "string:", z)
+
+
+
