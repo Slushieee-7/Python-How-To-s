da2737f7727d5454d3f27d1ce20f9e081d6ae413
Rey Delos Reyes <reyjoshua5922@gmail.com>
2025-01-15 15:27:30 +0800

    added part 5-8

--- Python Basics/Basics1.py
+++ Python Basics/Basics1.py
@@ -20,5 +20,39 @@ y = float(2.5)
 z = str(20)
 print ("Part 4:", "int:", x, "float:", y, "string:", z)
 
+#assigning mulitple values to multiple variables
+x, y, z = 1, 2, 3
+print ("Part 5:", x, y, z)
+
+#unpacking a collection
+fruits = ["watermelon", "apple", "banana", "grapes", "mango"]
+x, y, z, *other = fruits #we use *other to include other values in the collection
+basket = x, y, z, *other
+print ("Part 6A:", *other) #prints out grapes and mango 
+print ("Part 6B:", basket) 
+
+#Three numeric data types in python
+x = 9 #int
+y = 3.14 #float
+z = 3.14j #complex number
+
+#to check what data type the each variable is, use type(variable name):
+print ("Part 7A:", x, " is a ", type(x))
+print ("Part 7B:", y, " is a ", type(y))
+print ("Part 7C:", z, " is a ", type(z)) 
+
+#to convert these data types use:
+a = float(x) #converts int to float
+b = int(y) #converts float to int
+c = complex(x, y) #converts the int and float to complex
+print ("Part 8A:", a, " is a ", type(a))
+print ("Part 8B:", b, " is a ", type(b))
+print ("Part 8C:", c, " is a ", type(c)) #puts the two numbers, 9 and 3.14, into one parentheses then adds a "j" at the end of the last nnumber
+
+
+
+
+
+
 
 
