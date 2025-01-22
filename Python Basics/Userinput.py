c304eea63d2d25a30a3be147fadb9d518f2a2a93
Rey Delos Reyes <reyjoshua5922@gmail.com>
2025-01-22 14:13:42 +0800

    finish user input

---
+++ Python Basics/Userinput.py
@@ -0,0 +1,8 @@
+#this is how to user input in python
+#wtf ganun lang pala kadali
+
+#if u r using python 3.6:
+userinput = input("Enter something: ") 
+print("U have entered: " + userinput)
+
+#if u r using python 2.7, replace "input" with "raw_input"
